b9751fd046aded2e21423d5dc4ffd7adbda9dd4f
ElAwbery <36336917+ElAwbery@users.noreply.github.com>
2018-02-14 15:05:30 +0000

    Update beginner procedures2.py

--- beginner procedures2.py
+++ beginner procedures2.py
@@ -1,3 +1,9 @@
+'''
+created a procedure to open an external txt file
+started using Visual Studio Code to create txt & source code files
+'daysofweek' is ref to the external .txt file
+'''
+
 f = open ('daysofweek', 'r')
 
 firstline = f.readline()
