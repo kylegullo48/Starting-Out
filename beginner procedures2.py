9825b2048aaa4d005f944019d05200346d113126
ElAwbery <36336917+ElAwbery@users.noreply.github.com>
2018-02-15 14:12:53 +0000

    added procedures

--- beginner procedures2.py
+++ beginner procedures2.py
@@ -13,3 +13,23 @@ print (secondline)
 
 f.close()
 
+#using a for loop instead of readline to access, append and print text from an external file:
+
+f = open ('daysofweek', 'r+')
+for line in f:
+    print (line, end = '')
+
+f.close ()
+
+
+g = open ('daysofweek', 'a')
+g.write ('\nSaturday' '\nSunday')
+g.close ()
+
+
+h = open ('daysofweek', 'r+')
+for line in h:
+    print (line, end = '')
+h.close ()
+
+
