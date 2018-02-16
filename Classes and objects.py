f7d706ec08a954e9485d8cb5d7ee65130219d004
ElAwbery <36336917+ElAwbery@users.noreply.github.com>
2018-02-16 15:25:37 +0000

    added a method to the class

--- Classes and objects.py
+++ Classes and objects.py
@@ -13,12 +13,29 @@ class Book:     #self is a special parameter, must be included when initializing
 
     def __str__ (self):
         return "Book Title: %s, Author: %s, Year Published: %d" %(self.title, self.author, self.year)
+      
+    #a procedure insisde a class is referred to as a method
+    def calculateBookAge (self):
+         self.age = 2018 - self.year
+         return self.age
 
-#to create an object in the class:
+#to create an object in this class:
     
-Book1 = Book ('Foundation', 'Asimov', 1946)                         #Python includes the hidden self parameter
+Book1 = Book ('Foundation', 'Asimov', 1948)                         #Python automatically includes the hidden self parameter
 #The program returns 'creating a book file'
 
 print (Book1)
 #The program returns 'Book Title: Foundation, Author: Asimov, Year Published: 1946'
+
+#to call the calculateBookAge method
+
+Book1.calculateBookAge ()                                           #don't forget the parentheses
+#The program returns 70
+
+#To update any of the variables:
+Book1.year = 1950
+Book1.title = 'Foundation and Empire' 
+
+        
+        
  
