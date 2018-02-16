1e53f81228eddcf393e8812575a9d3a45ca55676
ElAwbery <36336917+ElAwbery@users.noreply.github.com>
2018-02-16 13:26:59 +0000

    Update Classes and objects.py

--- Classes and objects.py
+++ Classes and objects.py
@@ -1,9 +1,10 @@
 #creating a class (a class funcions as a template for an object)
 
-class Book:
-    #self is a special parameter, must be included when initializing variables inside a class
-    #variables in a class are referred to as instance variables
-    #initializing the instance variables 
+class Book:     #self is a special parameter, must be included when initializing variables inside a class
+                #variables in a class are referred to as instance variables
+    
+    #initializing the instance variables: 
+    
     def __init__(self, pBooktitle, pBookauthor, pYearpublished):    
         self.title = pBooktitle                                     #these are the instance variables
         self.author = pBookauthor                                   #always prefix instance variables with the self. keyword
